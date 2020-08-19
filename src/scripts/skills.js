cd3bf4a67fe020aebe671d630cc13626ed76a317
simplexone <61888881+simplexone@users.noreply.github.com>
2020-08-19 17:58:19 +0300

    Реализован компонент skills

--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -1,1 +1,42 @@
-console.log('this is the skills module');
+import Vue from  "vue";
+
+
+
+const skillsItem = {
+    props:["skill"],
+    template: "#skills-item",
+    mounted(){
+        const circle = this.$refs["colored-circle"];
+        const dashArray = parseInt(
+            getComputedStyle(circle).getPropertyValue("stroke-dasharray")
+        );
+
+        const percent = (dashArray/100) * (100 - this.skill.percent);
+
+        circle.style.strokeDashoffset = percent;
+    }
+};
+
+const skillsRow = {
+    props: ["category"],
+    template: "#skills-row",
+    components: {
+        skillsItem,
+    },
+};
+
+new Vue ({
+    el: "#skills-component",
+    template: "#skills-list",
+    components: {
+        skillsRow,
+    },
+    data(){
+        return {
+            skills: []
+        }
+    },
+    created(){
+        this.skills = require("../data/skills.json");
+    }
+});
