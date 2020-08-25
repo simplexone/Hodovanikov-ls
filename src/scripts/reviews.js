09b619fefa998ba8955dfb38cd8143dc41152257
simplexone <61888881+simplexone@users.noreply.github.com>
2020-08-25 11:12:17 +0300

    Сделан слайдер секции reviews

---
+++ src/scripts/reviews.js
@@ -0,0 +1,60 @@
+import Vue from "vue";
+import { Swiper, SwiperSlide,} from 'vue-awesome-swiper';
+import 'swiper/swiper-bundle.css';
+
+
+new Vue({
+    el: "#slider-component",
+    template: "#slider-container",
+    components: {
+        Swiper, SwiperSlide
+    },
+    data(){
+        return{
+            reviews: [],
+            sliderOptions: {
+                slidesPerView: 2,
+                loop: false
+            },
+            buttons: {
+                prev: false,
+                next: true
+            }
+        }
+    },
+    computed: {
+        slider(){
+            return this.$refs["slider"].$swiper;
+        }
+    },
+    methods: {
+        requireImagesToArray(data){
+            return data.map((item) => {
+                const requiredImage = require(`../images/content/${item.pic}`)
+                    .default;
+                item.pic = requiredImage;
+                return item;
+            });
+        },
+        slide(direction){
+            switch(direction){
+                case "next":
+                    this.slider.slideNext();
+                    break;
+                case "prev":
+                    this.slider.slidePrev();
+                    break;
+            } 
+        }
+    },
+    mounted() {
+        this.slider.on("slideChange", (swiper) => {
+            this.buttons.prev = !swiper.isBeginning;
+            this.buttons.next = !swiper.isEnd;
+        })
+    },
+    created(){
+        const data = require("../data/reviews.json");
+        this.reviews = this.requireImagesToArray(data)
+    }
+});
